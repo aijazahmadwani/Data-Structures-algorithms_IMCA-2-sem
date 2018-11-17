fb8a4e457c3636adb145a1575a2ef185b01a597a
aijazahmad911 <aijazahmad9864@gmail.com>
2018-11-17 22:40:39 +0530

    home

--- test/main.c
+++ test/main.c
@@ -3,10 +3,15 @@ email :aijazahmad9864@gmail.com
 IMCA (SEM-2)
 subject: DATA STRUCTURES*/
 #include <stdio.h>
-#include <stdlib.h>
-
 int main()
 {
+    int a, b,i;
+    scanf("%d\n%d", &a, &b);
+    char *s[] = {"one","two","three","four","five","six","seven","eight","nine"};
+    for(i=a;i<=b;i++)
+    {
+	if(i>=1 && i<=9)
+        printf("%s\n",s[i-1]);
 
-
+}
 }
