b818ef71b31483b6edf8ca720f0da5edf58a1a0e
aijazahmad911 <aijazahmad9864@gmail.com>
2018-12-19 22:47:01 +0530

    single linked list

---
+++ traverse_pointer_based_linked_list/main.c
@@ -0,0 +1,14 @@
+/* Author: Aijaz Ahmad Wani
+email :aijazahmad9864@gmail.com
+IMCA (SEM-2)
+Subject : DATA STRUCTURES*/
+#include <stdio.h>
+#include <stdlib.h>
+// traversing pointer based linked list
+struct node
+{
+    int data;
+    struct node* link;
+};
+
+
